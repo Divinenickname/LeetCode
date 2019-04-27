d111d1247dd9ec2b40bd7c8bfac16a53eace5dd7
Divinenickname <divinenickname@gmail.com>
2019-04-27 13:27:21 +0500

    Create Solution.java

---
+++ 206. Reverse Linked List/Solution.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while(current!=null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        
+        return prev;
+    }
+}
