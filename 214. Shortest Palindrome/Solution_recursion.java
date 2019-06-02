dfe6c3aa13106024c2822753b941f87bfc1d718b
Alexander Ilinykh <divinenickname@gmail.com>
2019-06-02 17:28:33 +0500

    Update Solution_recursion.java

--- 214. Shortest Palindrome/Solution_recursion.java
+++ 214. Shortest Palindrome/Solution_recursion.java
@@ -1,23 +1,24 @@
 class Solution {
-public String shortestPalindrome(String s) {
+        public String shortestPalindrome(String s) {
 
-        int n = s.length();
-        int i = 0;
-        for (int j = n-1; j >=0; j--) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
+                int n = s.length();
+                int i = 0;
+                for (int j = n-1; j >=0; j--) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        i++;
+                    }
+                }
+
+                // if string is palindrome
+                if (i == n) {
+                    return s;
+                }
+
+                String end = s.substring(i, n);
+                String front = new StringBuilder(end).reverse().toString();
+                //recursion here!
+                String mid = shortestPalindrome(s.substring(0,i));
+
+                return front + mid + end;
             }
-        }
-
-        // Если строка является палиндромом.
-        if (i == n) {
-            return s;
-        }
-
-        String end = s.substring(i, n);
-        String front = new StringBuilder(end).reverse().toString();
-        String mid = shortestPalindrome(s.substring(0,i));
-
-        return front + mid + end;
-    }
     }
